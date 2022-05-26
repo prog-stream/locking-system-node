6571567890dc9a0b41b1948a84335d12977fbbf2
FahadAly05 <fali13367@gmail.com>
2022-05-26 19:09:59 +0500

    Add mqtt constants

---
+++ src/config/constants.ts
@@ -0,0 +1,10 @@
+export const MQTT_TOPICS = {
+  LOCKS: "lock/#",
+  LOCK_STATUS: "lock/status",
+};
+
+export const MQTT_MESSAGES_TYPES = {
+  ALIVE: "alive",
+  OPENED: "opened",
+  DOOR: "door",
+};
